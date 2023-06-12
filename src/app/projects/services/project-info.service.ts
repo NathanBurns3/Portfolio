21554feb5bc5ac53bc28b0632f65220259a12ff2
Nathan Burns <nate10nate@gmail.com>
2023-06-11 20:18:09 -0400

    Added the Skills section

--- src/app/projects/services/project-info.service.ts
+++ src/app/projects/services/project-info.service.ts
@@ -39,7 +39,7 @@ export class ProjectInfoService {
         imageSrc: '../../../assets/project-images/SS.png',
         imageAlt: 'Student Scores GUI',
         projectTitle: 'Student Scores GUI',
-        toolsUsed: 'C# | .NET CORE | Git',
+        toolsUsed: 'C# | .NET Core | Git',
         projectDescription: [
           'Developed a .NET Core application that utilizes Entity Framework Core for database connectivity',
           'the application acts as a gradebook with student information, grades, calculated scores, and averages',
@@ -66,7 +66,7 @@ export class ProjectInfoService {
         imageSrc: '../../../assets/project-images/Async.png',
         imageAlt: 'Asynchronous Programming',
         projectTitle: 'Asynchronous Program',
-        toolsUsed: 'C# | .NET CORE | Git',
+        toolsUsed: 'C# | .NET Core | Git',
         projectDescription: [
           'Developed an application using .NET Core that takes in two text files and asynchronously processes each file',
           'Displays the 10 most used words for each book',
@@ -138,46 +138,3 @@ export class ProjectInfoService {
     ];
   }
 }
-
-/*
-**web api**
-	Developed a Web API using ASP.NET, comprising of 4 distinct controllers and utilizing Entity Framework Core to seamlessly connect to a database. Used NSwag library to view each controller and its actions
-	Implemented CRUD operations for each controller, ensuring seamless handling of all HTTP requests
-
-**endless runner**
-	Created a 2D Javascript came using the Phaser framework. Implemented two APIs, Geolocation and Weather, to fetch real-time data and alter the appearance of the game.
-
-**student scores gui**
-	Developed an application using .NET CORE that utilizes Entity Framework Core to connect to a database
-	That application acts as a gradebook comprising of students and grades with calcuated scores and averages for each student. Users and able to add, edit, and delete students from the database
-	Implemented the ICloneable Interface to create deep copies of student objects when updating a student. This allows the changes to only be saved to the current student when the user accepts the changes
-	The application is built with encapsulated forms allowing you to easily monever between operations
-
-**name api**
-Developed a web api using ASP.NET and Entity Framework Core for database connection
-	Implemented a GET HTTP request that returns a name
-
-**asynchronous programming**
-	Developed an application using .NET Core that takes in two text file and asynchronously processes each file and displays the 10 most used works for each book
-
-**recursive file lister**
-	Created a JSwing application that uses JFileChooser to allow users to select a directory in the system. It then uses a recurive approach to list all the files within the chose directory and any of it's sub-directories.
-
-**binary search gui**
-	Developed a custom binary search algorithm to efficiently locate a string within an array and return its position
-	Facilitated user input of a series of strings, sorting them in ascending order for optimal searchability
-	In cases where the requested string was not present in the array, the algorithm provided the position it would occupy if it were present, thus allowing for more efficient future input data
-
-**ski mountain website**
-	Designed and developed a responsive website using HTML, CSS, and JavaScript, incorporating animations and interactive features
-	Conducted research on ski resorts across the US and gathered data to assist individuals in selecting suitable ski packages
-
-**vex turning point**
-	Worked with a team to design a robot that would compete against other classmates in a fast-pace point system game
-	Maped buttons on a controller to different motors and gears on the robot
-	Coded an autonoumous program in RobotC that would set the robot in position and drive up different levels of platforms
-
-**vex marble sorter**
-	Given a task to design and construct a machine to help recycle matericals
-	Coded a program in RobotC that would sense the light given off an object and sort it inot the assigned slot
-*/
