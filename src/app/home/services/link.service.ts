4c11595dc1f4d0a32c71290185aa5421b34a916d
Nathan Burns <nate10nate@gmail.com>
2023-06-17 07:26:53 -0400

    fixed image sources

--- src/app/home/services/link.service.ts
+++ src/app/home/services/link.service.ts
@@ -9,23 +9,23 @@ export class LinkService {
     return [
       {
         url: 'https://www.linkedin.com/in/nathan-burns-717802170/',
-        iconSrc: '/assets/social-media-icons/linkedin-white.png',
-        iconSrcHover: '/assets/social-media-icons/linkedin-green.png',
-        iconSrcDefault: '/assets/social-media-icons/linkedin-white.png',
+        iconSrc: './assets/social-media-icons/linkedin-white.png',
+        iconSrcHover: './assets/social-media-icons/linkedin-green.png',
+        iconSrcDefault: './assets/social-media-icons/linkedin-white.png',
         altText: 'LinkedIn',
       },
       {
         url: 'https://www.github.com/NathanBurns3',
-        iconSrc: '/assets/social-media-icons/github-white.png',
-        iconSrcHover: '/assets/social-media-icons/github-green.png',
-        iconSrcDefault: '/assets/social-media-icons/github-white.png',
+        iconSrc: './assets/social-media-icons/github-white.png',
+        iconSrcHover: './assets/social-media-icons/github-green.png',
+        iconSrcDefault: './assets/social-media-icons/github-white.png',
         altText: 'GitHub',
       },
       {
         url: 'https://www.instagram.com/nathan.burnss/',
-        iconSrc: '/assets/social-media-icons/instagram-white.png',
-        iconSrcHover: '/assets/social-media-icons/instagram-green.png',
-        iconSrcDefault: '/assets/social-media-icons/instagram-white.png',
+        iconSrc: './assets/social-media-icons/instagram-white.png',
+        iconSrcHover: './assets/social-media-icons/instagram-green.png',
+        iconSrcDefault: './assets/social-media-icons/instagram-white.png',
         altText: 'Instagram',
       },
     ];
