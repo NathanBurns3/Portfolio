62b05bf9c363674bc3ad9e3bb76e8c495cd20786
Nathan Burns <nate10nate@gmail.com>
2025-08-21 23:57:02 -0400

    Update image file extensions for consistency in portrait photo service

--- src/app/about-me/services/portrait-photos.service.ts
+++ src/app/about-me/services/portrait-photos.service.ts
@@ -8,35 +8,35 @@ export class PortraitPhotoService {
   getPortraitPhotos(): IPortraitPhoto[] {
     return [
       {
-        imageSrc: './assets/profile-pictures/IMG1.JPG',
+        imageSrc: './assets/profile-pictures/IMG1.jpg',
         imageAlt: 'Prom',
       },
       {
-        imageSrc: './assets/profile-pictures/IMG2.JPG',
+        imageSrc: './assets/profile-pictures/IMG2.jpg',
         imageAlt: 'Snowboarding',
       },
       {
-        imageSrc: './assets/profile-pictures/IMG3.JPG',
+        imageSrc: './assets/profile-pictures/IMG3.jpg',
         imageAlt: 'Close up',
       },
       {
-        imageSrc: './assets/profile-pictures/IMG4.JPG',
+        imageSrc: './assets/profile-pictures/IMG4.jpg',
         imageAlt: 'Close up',
       },
       {
-        imageSrc: './assets/profile-pictures/IMG5.JPG',
+        imageSrc: './assets/profile-pictures/IMG5.jpg',
         imageAlt: 'Cross Country Districts',
       },
       {
-        imageSrc: './assets/profile-pictures/IMG6.JPG',
+        imageSrc: './assets/profile-pictures/IMG6.jpg',
         imageAlt: 'Close up',
       },
       {
-        imageSrc: './assets/profile-pictures/IMG7.JPG',
+        imageSrc: './assets/profile-pictures/IMG7.jpg',
         imageAlt: 'Close up',
       },
       {
-        imageSrc: './assets/profile-pictures/IMG8.JPG',
+        imageSrc: './assets/profile-pictures/IMG8.jpg',
         imageAlt: 'Cross Country State Championship',
       },
     ];
